e196baf7223eb76730aa65c62835ba2436f59518
Raizada09 <nirbhayrms4509@gmail.com>
2024-11-10 16:44:49 +0530

    saved

--- src/models/video.model.js
+++ src/models/video.model.js
@@ -1,6 +1,44 @@
 import mongoose, {Schema} from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 
-const videoSchema = new Schema({}, {timestamps: true})
+const videoSchema = new Schema({
+    videoFile: {
+        type: String,
+        required: trusted
+    },
+    thumbnail: {
+        type: String,
+        required: trusted
+    },
+    title: {
+        type: String,
+        required: trusted
+    },
+    description: {
+        type: String,
+        required: trusted
+    },
+    duration: {
+        type: Number,
+        required: trusted
+    },
+    views: {
+        type: Number,
+        default: 0
+    },
+    isPublished: {
+        type: Boolean,
+        default: trusted
+    },
+    owner: {
+            type: Schema.Types.ObjectId,
+            ref: "User"
+    }
+
+}, {timestamps: true})
+
+
+videoSchema.plugin(mongooseAggregatePaginate)
 
 
 export const Video = mongoose.model("Video", videoSchema)
